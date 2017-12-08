0eb25b51847bbe4f79f163dd7a9da088e16148c6
Karol Patecki <karol.patecki09@gmail.com>
2017-12-08 16:03:53 +0100

    Do poprawy

--- task3.java
+++ task3.java
@@ -30,35 +30,63 @@ public class task3 {
 		int max=1000;
 		int min=0;
 		Random rand = new Random ();
-		String less = "M";
-		String more = "W";
-		String bingo = "B";
-		String option[]={"M","W","B"};
-		Arrays.sort(option);
-		
-		int contain =1;
-		boolean win=false;
+//		String less = "M";
+//		String more = "W";
+//		String bingo = "B";
+//		String option[]={"M","W","B"};
+//		Arrays.sort(option);
+//		
+//		int contain =1;
+    	boolean win=false;
 		int guess = 0;
+		String answer = "";
+		String answer1 = "";
 			
 		while (win!=true){
 			System.out.println(max);
 			System.out.println(min);
 			//guess=rand.nextInt(((max - min)+1)+min);
 			guess=ThreadLocalRandom.current().nextInt(min, max + 1);
-			System.out.println("Zgaduję: "+ guess + " Jeśli więcej napisz W, jeśli mniej napisz M, jeśli trafiłem napisz B");
+			System.out.println("Zgaduję: "+ guess + " Zgadłem? Napisz \"Tak\" lub \"Nie\"");
 			//String word=scan.nextLine();	
 			
 //			while(scan.nextLine().length()!=1){
 //				System.out.println("Miało być W, M lub B");
 //				scan.next();
 //				}
-			
-			String word=scan.next();
-			contain = Arrays.binarySearch(option, word);
-			while(contain>0){
-				System.out.println("Błędne hasło");
+			String word = scan.next();
+			if(word.equals("Tak")){
+				System.out.println("Super udało się");
+				win=true;
+				
+			}else if(word.equals("Nie")){
+					answer=scan.next();
+					System.out.println("Więcej?");
+					
+					if(answer.equals("Tak")){
+						min=guess;
+						
+					}else if (answer.equals("Nie")){
+						System.out.println("Mniej?");
+						answer1=scan.next();
+						
+						if(answer1.equals("Tak")){
+							max=guess;
+						}else if(answer1.equals("Nie")){
+							System.out.println("Nie oszukuj!");
+							break;
+						}
+					}
+			}else {
+				System.out.println("Wprowadzono złe hasło. Podaj jeszcze raz.");
 				scan.next();
 			}
+//			String word=scan.next();
+//			contain = Arrays.binarySearch(option, word);
+//			while(contain>0){
+//				System.out.println("Błędne hasło");
+//				scan.next();
+//			}
 //			while(word.length()<1 && word.length()>1 && (!word.equals(more) || !word.equals(less) || !word.equals(bingo))){
 //				System.out.println("Błędne hasło");
 //				scan.next();
@@ -70,15 +98,16 @@ public class task3 {
 //				throw new Exception ("Błędne hasło");
 //			}
 			
-			if(word.equals(more)){
-				min=guess;
-			}else if(word.equals(less)){
-				max=guess;
-			}else if(word.equals(bingo)){
-				System.out.println("Zgadłem!");
-				win=true;
-			}
-		}
+//			if(word.equals(more)){
+//				min=guess;
+//			}else if(word.equals(less)){
+//				max=guess;
+//			}else if(word.equals(bingo)){
+//				System.out.println("Zgadłem!");
+//				win=true;
+//			}
+//		}
+	}
 	}
 
 }
